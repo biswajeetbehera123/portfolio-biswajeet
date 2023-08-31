a4233fba5900ea7be38c730aea24eb01ccd37350
biswajeetbehera123 <107670354+biswajeetbehera123@users.noreply.github.com>
2023-08-31 11:03:40 +0530

    analytics added

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import Navbar from '@/components/Navbar'
 import './globals.css'
 import { Inter } from 'next/font/google'
 // import Providers from './providers'
+import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -17,8 +18,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-          <Navbar />
-          {children}
+        <Navbar />
+        {children}
+        <Analytics />
       </body>
     </html>
 
